80fb139c83824c056a3a94247b34655c7b7c3653
JudeTruong <162196118+JudeTruong@users.noreply.github.com>
2026-01-29 16:43:42 -0500

    getting sorted
    updated file structure and added 3d model glb

--- src/main.js
+++ src/main.js
@@ -1,9 +1,31 @@
 import * as THREE from 'three';
-import { PlayerControls } from './classes/PlayerControls.js';
+import { PlayerControls } from './player/PlayerControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 /* Scene */
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb);
+
+const loader = new GLTFLoader();
+
+loader.load(
+  '/models/NPCHallway.glb',
+  (gltf) => {
+    const model = gltf.scene;
+
+    model.scale.set(100, 100, 100);
+    model.position.set(0, 0, -5);
+    model.rotation.y = Math.PI; // optional
+
+    scene.add(model);
+  },
+  undefined,
+  (error) => {
+    console.error('Error loading model:', error);
+  }
+);
+
+
 
 /* Camera */
 const camera = new THREE.PerspectiveCamera(
@@ -20,7 +42,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 /* Lights */
-scene.add(new THREE.DirectionalLight(0xffffff, 1));
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(10, 20, 10); // coming from top-right
+scene.add(light);
+const helper = new THREE.DirectionalLightHelper(light, 5);
+scene.add(helper);
+
 scene.add(new THREE.AmbientLight(0xffffff, 0.4));
 
 /* Floor */
@@ -36,7 +64,7 @@ const cube = new THREE.Mesh(
   new THREE.BoxGeometry(12, 13, 12),
   new THREE.MeshStandardMaterial({ color: 0xF54927 })
 );
-cube.position.set(0, 1, -5);
+cube.position.set(0, 15, -5);
 scene.add(cube);
 
 /* Player */
@@ -60,6 +88,7 @@ function animate() {
   player.update();
   cube.rotation.y += 0.05;
   cube.rotation.x += 0.05;
+
   
 
   renderer.render(scene, camera);
